b4758e09c6beb9bcbe276637646cbcde8680ffa7
davide-ferreri <dferreri45@gmail.com>
2026-04-23 11:44:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 import torch
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 from modules.data_pipeline import GeoDataFetcher
 from modules.ai_engine import SolarUNet
